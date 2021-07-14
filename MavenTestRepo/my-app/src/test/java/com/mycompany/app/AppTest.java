4b29e65c1242aa12cbb5c7afd49e591c6c9ce306
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-07-14 19:40:58 +0200

    end of 14.07.21

--- MavenTestRepo/my-app/src/test/java/com/mycompany/app/AppTest.java
+++ MavenTestRepo/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,40 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import static org.junit.Assert.*;
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void checkGreeting()
     {
-        assertTrue( true );
+        String checkGreeting = App.greeting();
+        assertTrue(checkGreeting.length()<10);
+        assertTrue(checkGreeting.contains("oin"));
+        assertFalse(checkGreeting.startsWith("H"));
     }
+
+    @Test
+    public void testLengthOfString(){
+        // GIVEN
+        String theText = "Deine Mama";
+        // WHEN
+        int actual = App.calculateLengthOfString(theText);
+        // THEN
+        assertTrue(actual<20 && actual>4);
+    }
+
+    @Test
+    public void testAdd (){
+        // GIVEN
+        int firstValue = 3;
+        int secondValue = 7;
+        // WHEN
+        int actual = App.add(firstValue, secondValue);
+        // THEN
+        assertEquals(10, actual);
+    }
+
+
 }
