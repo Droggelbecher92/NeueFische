e7e430e30f6a57a8562057a0ac53feebb2558551
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-07-14 12:09:44 +0200

    added Strings.java

--- 14.07.21_1/src/Methoden.java
+++ 14.07.21_1/src/Methoden.java
@@ -1,9 +1,26 @@
 public class Methoden {
-    public static int duplicate(int value){
-        return value*2;
-    }
 
     public static void main(String[] args) {
         System.out.println(duplicate(5));
+        System.out.println(add(4, 6));
+        System.out.println(sub(5, 2));
+        System.out.println(div(15, 3));
     }
+
+    public static int duplicate(int value) {
+        return value * 2;
+    }
+
+    public static int add(int fistValue, int secondValue) {
+        return fistValue + secondValue;
+    }
+
+    public static int sub(int fistValue, int secondValue) {
+        return fistValue - secondValue;
+    }
+
+    public static int div(int fistValue, int secondValue) {
+        return fistValue / secondValue;
+    }
+
 }
---
+++ 14.07.21_1/src/Strings.java
@@ -0,0 +1,7 @@
+public class Strings {
+    public static void main(String[] args) {
+        String letter = "o";
+        boolean result = "Hallo".contains(letter);
+        System.out.println(result);
+    }
+}
