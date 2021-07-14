eff8037f504be3f8f5b24e941a08f8ee16b6db12
Thomas Kittlaus <77334737+Droggelbecher92@users.noreply.github.com>
2021-07-14 09:51:23 +0200

    Add files via upload

---
+++ rem21-3/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int num1 = 4;
+        int num2 = 38;
+
+        System.out.println("Die Antwort auf alles ist "+(num1+num2)+"!");
+    }
+}
